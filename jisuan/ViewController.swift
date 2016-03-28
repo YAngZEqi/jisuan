d8a0b968e09de15622f9a5135134f0a90992becb
YAngZEqi <yangzeqi@Mac12.local>
2016-03-28 19:04:56 +0800

    计算器的雏形
    画出计算器的框

--- jisuan/ViewController.swift
+++ jisuan/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var resultLabel: UILabel!
+
+    @IBOutlet weak var opeRatorLabel: UILabel!
+    var opeRand1: String = ""
+    var opeRand2: String = ""
+    var opeRator: String = ""
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +26,71 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    @IBAction func didClicked(sender: UIButton) {
+        var value = sender.currentTitle
+        let bool_value = true
+        if  value == "+"||value == "-"||value == "×"||value == "÷"||value == "%"||value == "^"
+        {
+            opeRator = value!
+            opeRatorLabel.text = "\(opeRator)"
+            return
+        }
+        else if value == "AC"{
+            value = "0"
+            opeRand1 = ""
+            opeRand2 = ""
+            opeRator = ""
+            resultLabel.text = "0"
+            opeRatorLabel.text = ""
+            return
+        }
+        else if value == "+/-"{
+            if  bool_value ==  true{
+                bool_value == false
+            }
+            else{
+                bool_value == true
+            }
+        }
+        else if value == "="{
+            var result = 0
+            switch opeRator {
+            case "+":
+                result = Int(opeRand1)! + Int(opeRand2)!
+            case "-":
+                result = Int(opeRand1)! - Int(opeRand2)!
+            case "×":
+                result = Int(opeRand1)! * Int(opeRand2)!
+            case "÷":
+                result = Int(opeRand1)! / Int(opeRand2)!
+            case "%":
+                result = Int(opeRand1)! % Int(opeRand2)!
+            case "^":
+                result = Int(opeRand1)! ^ Int(opeRand2)!
+                
+            default :
+                resultLabel.text = "出错请清零"
+            }
+            resultLabel.text  = "\(result)"
+            opeRatorLabel.text = ""
+            opeRand1 = ""
+            opeRand2 = ""
+            opeRator = ""
+            
+            return
+        }
+        if opeRator   == ""{
+            
+            opeRand1  = opeRand1 + value!
+            resultLabel.text = "\(opeRand1)"
+            return
+        }
+        else {
+            
+            opeRand2  = opeRand2 + value!
+            resultLabel.text = "\(opeRand2)"
+            return
+        }
+    }
 }
 
